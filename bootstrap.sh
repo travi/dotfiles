571473ce21a6e9e0faf5a751f4da7d6f88b33d9b
Matt Travi <programmer@travi.org>
2014-04-24 21:46:09 -0500

    more output functions

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
 info () {
-	  printf "  [ \033[00;34m..\033[0m ] $1"
+    printf "  [ \033[00;34m..\033[0m ] $1"
+}
+
+success () {
+    printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+}
+
+fail () {
+    printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
+    echo ''
+    exit
 }
 
 info 'Brew bundle'
