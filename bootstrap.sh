01e41c6f28db2115c71d3bbe5a747c6bbec13b31
Matt Travi <programmer@travi.org>
2014-05-03 07:58:17 -0500

    break line after output

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,10 +4,12 @@ DOTFILES_ROOT="`pwd`"
 
 info () {
     printf "  [ \033[00;34m..\033[0m ] $1"
+    echo ''
 }
 
 success () {
     printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+    echo ''
 }
 
 fail () {
