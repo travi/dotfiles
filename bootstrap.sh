2b9e61190917f6465c4de02a7a65ddb01aa81f06
Matt Travi <TraviMatthewJ@JohnDeere.com>
2014-05-20 12:48:53 -0500

    removed whitespace output

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,12 +9,10 @@ info () {
 
 success () {
     printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
-    echo ''
 }
 
 fail () {
     printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
-    echo ''
     exit
 }
 
