9d98aae8ae392195be7bcdc5d5c84d117383a719
Matt Travi <programmer@travi.org>
2014-07-28 18:35:28 -0500

    added aliases for showing and hiding hidden files in finder

---
+++ aliases.sh
@@ -0,0 +1,2 @@
+alias showHidden='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
+alias hideHidden='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
