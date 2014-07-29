1e1c91d4ed23766bcec4ecfb1327ff87b9129bc8
Matt Travi <programmer@travi.org>
2014-07-29 18:05:54 -0500

    added a source script for sourcing all the common scripts

---
+++ source.sh
@@ -0,0 +1,4 @@
+. ~/.dotfiles/colors.sh
+. ~/.dotfiles/completion.sh                                                                                                                                     
+. ~/.dotfiles/aliases.sh
+. ~/.dotfiles/exports.sh
