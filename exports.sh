9e5d772af5a96c0a1e361b383b0ea3a779ea54a1
Matt Travi <programmer@travi.org>
2014-07-28 19:04:55 -0500

    added exports file for #8

---
+++ exports.sh
@@ -0,0 +1,5 @@
+export CLICOLOR=1
+
+export PATH=/usr/local/bin:$(brew --prefix ruby)/bin:$PATH
+export M2_HOME=$(brew --prefix maven31)/libexec
+export PYTHONPATH=$(brew --prefix mercurial)/lib/python2.7/site-packages:$(brew --prefix mercurial)/lib/python2.7/site-packages/mercurial:$PYTHONPATH
