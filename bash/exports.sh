669e01ee1b96671252ba4193de6d3f2ba23cd48e
Matt Travi <programmer@travi.org>
2014-07-30 22:06:47 -0500

    ensured that environment variables that use brew relative paths are only set on osx. closes #10

--- bash/exports.sh
+++ bash/exports.sh
@@ -1,9 +1,6 @@
 export CLICOLOR=1
 
-export PATH=/usr/local/bin:$(brew --prefix ruby)/bin:$PATH
-export M2_HOME=$(brew --prefix maven31)/libexec
-export M2="${M2_HOME}/bin"
-export PYTHONPATH=$(brew --prefix mercurial)/lib/python2.7/site-packages:$(brew --prefix mercurial)/lib/python2.7/site-packages/mercurial:$PYTHONPATH
+. ../osx/exports.sh
 
 # Make vim the default editor
 export EDITOR="vim";
---
+++ osx/exports.sh
@@ -0,0 +1,7 @@
+# OSX-only stuff. Abort if not OSX.
+[[ "$OSTYPE" =~ ^darwin ]] || return 1
+
+export PATH=/usr/local/bin:$(brew --prefix ruby)/bin:$PATH
+export M2_HOME=$(brew --prefix maven31)/libexec
+export M2="${M2_HOME}/bin"
+export PYTHONPATH=$(brew --prefix mercurial)/lib/python2.7/site-packages:$(brew --prefix mercurial)/lib/python2.7/site-packages/mercurial:$PYTHONPATH
