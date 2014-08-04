4bff8af1e0ce6a45f4f3f78b22cf946f0757a2ad
Matt Travi <programmer@travi.org>
2014-08-03 22:02:43 -0500

    colors for the prompt for #19

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -9,11 +9,11 @@ function prompt_command() {
 
   PS1="\n"
   # path: [user@host:path]
-  PS1="$PS1$c1[$c0\u$c1@$c0\h$c1:$c0\w$c1]$c9"
-  
+  PS1="$PS1${white}[${blue}\u${white}@${blue}\h${white}:${blue}\w${white}]${reset}"
+
   PS1="$PS1\n"
   # date: [HH:MM:SS]
-  PS1="$PS1$c1[$c0$(date +"%H$c1:$c0%M$c1:$c0%S")$c1]$c9"
+  PS1="$PS1${white}[${blue}$(date +"%H${white}:${blue}%M${white}:${blue}%S")${white}]${reset}"
   # exit code: 127
   PS1="$PS1$(prompt_exitcode "$exit_code")"
   PS1="$PS1 \$ "
