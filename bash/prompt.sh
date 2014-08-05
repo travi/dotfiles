36e3bf8691d6e4f0d4c03b3e3da1269eef9d7021
Matt Travi <programmer@travi.org>
2014-08-04 20:05:52 -0500

    used \[ and \] escape characters to help bash determine the proper line length. fixes #20

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -1,19 +1,21 @@
 # Exit code of previous command.
 function prompt_exitcode() {
 #  prompt_getcolors
-  [[ $1 != 0 ]] && echo " ${red}$1${reset}"
+  [[ $1 != 0 ]] && echo " \[${red}\]$1\[${reset}\]"
 }
 
 function prompt_command() {
   local exit_code=$?
 
+  local prompt_color=${green}
+
   PS1="\n"
   # path: [user@host:path]
-  PS1="$PS1${white}[${blue}\u${white}@${blue}\h${white}:${blue}\w${white}]${reset}"
+  PS1="$PS1${white}[$prompt_color\u${white}@$prompt_color\h${white}:$prompt_color\w${white}]${reset}"
 
   PS1="$PS1\n"
   # date: [HH:MM:SS]
-  PS1="$PS1${white}[${blue}$(date +"%H${white}:${blue}%M${white}:${blue}%S")${white}]${reset}"
+  PS1="$PS1\[${white}\][$(date +"\[$prompt_color\]%H\[${white}\]:\[$prompt_color\]%M\[${white}\]:\[$prompt_color\]%S")\[${white}\]]\[${reset}\]"
   # exit code: 127
   PS1="$PS1$(prompt_exitcode "$exit_code")"
   PS1="$PS1 \$ "
