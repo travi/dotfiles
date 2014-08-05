7e29b9ce330c4f287c21058cedb464c0844c04f2
Matt Travi <programmer@travi.org>
2014-08-04 22:43:19 -0500

    pulled escape characters up to the variables for #19

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -7,15 +7,16 @@ function prompt_exitcode() {
 function prompt_command() {
   local exit_code=$?
 
-  local prompt_color=${green}
+  local prompt_color="\[${green}\]"
+  local bracket_color="\[${white}\]"
 
   PS1="\n"
   # path: [user@host:path]
-  PS1="$PS1${white}[$prompt_color\u${white}@$prompt_color\h${white}:$prompt_color\w${white}]${reset}"
+  PS1="$PS1$bracket_color[$prompt_color\u${white}@$prompt_color\h${white}:$prompt_color\w$bracket_color]${reset}"
 
   PS1="$PS1\n"
   # date: [HH:MM:SS]
-  PS1="$PS1\[${white}\][$(date +"\[$prompt_color\]%H\[${white}\]:\[$prompt_color\]%M\[${white}\]:\[$prompt_color\]%S")\[${white}\]]\[${reset}\]"
+  PS1="$PS1$bracket_color[$(date +"$prompt_color%H\[${white}\]:$prompt_color%M\[${white}\]:$prompt_color%S")$bracket_color]\[${reset}\]"
   # exit code: 127
   PS1="$PS1$(prompt_exitcode "$exit_code")"
   PS1="$PS1 \$ "
