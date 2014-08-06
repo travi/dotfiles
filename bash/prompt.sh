13ea3b4455e9249af6e28e9312cc309557cfcfd9
Matt Travi <programmer@travi.org>
2014-08-05 19:26:49 -0500

    included cowboy's git status function

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -1,3 +1,29 @@
+function prompt_git() {
+  prompt_getcolors
+  local status output flags
+  status="$(git status 2>/dev/null)"
+
+  [[ $? != 0 ]] && return;
+
+  output="$(echo "$status" | awk '/# Initial commit/ {print "(init)"}')"
+
+  [[ "$output" ]] || output="$(echo "$status" | awk '/# On branch/ {print $4}')"
+  [[ "$output" ]] || output="$(git branch | perl -ne '/^\* (.*)/ && print $1')"
+
+  flags="$(
+    echo "$status" | awk 'BEGIN {r=""} \
+      /^# Changes to be committed:$/        {r=r "+"}\
+      /^# Changes not staged for commit:$/  {r=r "!"}\
+      /^# Untracked files:$/                {r=r "?"}\
+      END {print r}'
+    )"
+
+    if [[ "$flags" ]]; then
+      output="$output$c1:$c0$flags"
+    fi
+      echo "$c1[$c0$output$c1]$c9"
+}
+
 # Exit code of previous command.
 function prompt_exitcode() {
 #  prompt_getcolors
