68383467aea08a239825054f152a7d69f75a4d66
Matt Travi <programmer@travi.org>
2014-08-14 07:16:15 -0500

    moved osx bash completion to osx script for #31

--- bash/completion.sh
+++ bash/completion.sh
@@ -1,8 +1,6 @@
-if [ -f $(brew --prefix)/etc/bash_completion ]; then
-        . $(brew --prefix)/etc/bash_completion
-fi
-
 # SSH auto-completion based on entries in known_hosts.
 if [[ -e ~/.ssh/known_hosts ]]; then
 	complete -o default -W "$(cat ~/.ssh/known_hosts | sed 's/[, ].*//' | sort | uniq | grep -v '[0-9]')" ssh scp sftp
 fi
+
+. ~/.dotfiles/osx/completion.sh
