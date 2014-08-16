aa3f9b1d2700864ce147e905713b4dc68f87b62c
Matt Travi <programmer@travi.org>
2014-08-16 17:17:55 -0500

    some refactoring to reduce duplication before starting #14

--- functions.sh
+++ functions.sh
@@ -4,83 +4,80 @@ DOTFILES_ROOT="`pwd`"
 
 . "$DOTFILES_ROOT/bash/colors.sh"
 
-heading () {
-    printf "\n         ${yellow}$1${reset}\n"
+heading() {
+    printf "\n${yellow}$1${reset}\n"
 }
 
-info () {
-     printf "  [ ${blue}..${reset} ] $1\n"
+info() {
+    printf "  [ ${blue}..${reset} ] $1\n"
 }
 
-success () {
+success() {
     printf "  [ ${green}OK${reset} ] $1\n"
 }
 
-warn () {
+warn() {
     printf "  [${orange}WARN${reset}] $1\n"
 }
 
-fail () {
+fail() {
     printf "  [${red}FAIL${reset}] $1\n"
     exit
 }
 
-link_file () {
-    ln -s $1 $2
-    success "linked $1 to $2"
+link_file() {
+    source=$1
+    dest=$2
+
+    # is a broken symbolic link
+    if [ -h ${dest} ] && [ ! -e ${dest} ]; then
+        warn "Removing broken link for ${dest}"
+        rm ${dest}
+    fi
+
+    # is a symbolic link
+    if [ -h ${dest} ]; then
+        info "${dest} already linked"
+    # is a real file or directory
+    elif [ -f ${dest} ] || [ -d ${dest} ]; then
+        warn "${dest} already exists"
+    # safe to create symbolic link
+    else
+        ln -s ${source} ${dest}
+        success "linked ${source} to ${dest}"
+    fi
 }
 
-dotfile () {
+dotfile() {
     source=$1
     filename=`basename $source`
     dest="$HOME/$filename"
 
-    # is a broken symbolic link
-    if [ -h $dest ] && [ ! -e $dest ];then
-        warn "Removing broken link for $dest"
-        rm $dest
-    fi
-
-    # is a symbolic link
-    if [ -h $dest ];then
-	    info "link $dest already exists"
-    # is a real file or directory
-    elif [ -f $dest ] || [ -d $dest ]
-    then
-	    warn "$dest already exists"
-    # safe to create symbolic link
-    else
-	    link_file $source $dest
-    fi
+    link_file ${source} ${dest}
 }
 
 link_dotfiles_directory() {
-	if [ -h "$HOME/.dotfiles" ]
-	then
-		info ".dotfiles already linked"
-	else
-		link_file $DOTFILES_ROOT "$HOME/.dotfiles"
-	fi
+    link_file ${DOTFILES_ROOT} "$HOME/.dotfiles"
 }
 
 link_dotfiles() {
-	DOTFILES_LINK="$HOME/.dotfiles/"
+    DOTFILES_LINK="$HOME/.dotfiles/"
 
-	for source in `find $DOTFILES_LINK -mindepth 2 -maxdepth 2 -name '\.*'`
-	do
-		dotfile ${source}
-	done
+    for source in `find ${DOTFILES_LINK} -mindepth 2 -maxdepth 2 -name '\.*'`
+    do
+        dotfile ${source}
+    done
 }
 
 source_scripts() {
     local profile="$HOME/.bash_profile"
 
-    if [ -e $profile ]; then
+    if [ -e ${profile} ]; then
         info ".bash_profile already exists"
     else
-        touch $profile
-        echo ". $HOME/.dotfiles/bash/source.sh" >> $profile
-        . $profile
+        touch ${profile}
+        echo ". $HOME/.dotfiles/bash/source.sh" >> ${profile}
+        . ${profile}
         success "added .bash_profile and sourced configuration files"
     fi
 }
