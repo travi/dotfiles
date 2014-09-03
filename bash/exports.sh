e14101b4442ed42ffbeaf8513c77d6312e7068dc
Matt Travi <programmer@travi.org>
2014-09-02 20:37:08 -0500

    more color adjustments for #41

--- bash/exports.sh
+++ bash/exports.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-export CLICOLOR=1
-
-. ~/.dotfiles/osx/exports.sh
+. $HOME/.dotfiles/osx/exports.sh
 
 # Make vim the default editor
 export EDITOR="vim";
@@ -11,15 +9,7 @@ export EDITOR="vim";
 export LANG="en_US.UTF-8";
 export LC_ALL="en_US.UTF-8";
 
-export LESSOPEN="| src-hilite-lesspipe.sh %s"
-export LESS=-R
-# Highlight section titles in manual pages
-export LESS_TERMCAP_md="${yellow}";
-
 # Don’t clear the screen after quitting a manual page
 #export MANPAGER="less -X";
 export MANPAGER="less";
-
-# Always enable colored `grep` output
-export GREP_OPTIONS="--color=auto";
 
