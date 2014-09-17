2524d56963885319c9c7471d13e96627e19d48f0
Matt Travi <github@travi.org>
2014-09-17 12:55:09 -0500

    added utf-8 apple logo

--- osx/environment.sh
+++ osx/environment.sh
@@ -3,7 +3,7 @@
 # OSX-only stuff. Abort if not OSX.
 [[ "$OSTYPE" =~ ^darwin ]] || return 1
 
-heading 'Configuring OS X'
+heading 'Configuring OS X '
 
 # Ask for the administrator password upfront
 sudo -v
