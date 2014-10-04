f9d97d81a5285e31fed299d17503baf09385008f
Matt Travi <programmer@travi.org>
2014-10-04 08:48:29 -0500

    updated source of battery charge information to be in sync with what is shown by osx for #43

--- tmux/battery_charge_indicator.sh
+++ tmux/battery_charge_indicator.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
 if [[ `uname` == 'Linux' ]]; then
-    current_charge=$(cat /proc/acpi/battery/BAT1/state | grep 'remaining capacity' | awk '{print $3}')
-    total_charge=$(cat /proc/acpi/battery/BAT1/info | grep 'last full capacity' | awk '{print $4}')
+    battery=$(upower -e | grep battery | head -1)
+    upower -i $battery | grep percentage | awk '{print $2}'
 else
-    battery_info=`ioreg -rc AppleSmartBattery`
-    current_charge=$(echo $battery_info | grep -o '"CurrentCapacity" = [0-9]\+' | awk '{print $3}')
-    total_charge=$(echo $battery_info | grep -o '"MaxCapacity" = [0-9]\+' | awk '{print $3}')
+    pmset -g batt | awk 'NR==2 { gsub(/;/,""); print $2 }'
 fi
 
-charge_percent=$((${current_charge} * 100 / ${total_charge}))
-echo $charge_percent
