ad7b5f292993cc81379bb2625dc82417d12e969c
Matt Travi <TraviMatthewJ@JohnDeere.com>
2014-11-06 12:24:36 -0600

    allowed maven to be updated beyond 3.1

--- osx/exports.sh
+++ osx/exports.sh
@@ -4,7 +4,7 @@
 [[ "$OSTYPE" =~ ^darwin ]] || return 1
 
 export PATH=/usr/local/bin:$(brew --prefix ruby)/bin:$PATH
-export M2_HOME=$(brew --prefix maven31)/libexec
+export M2_HOME=$(brew --prefix maven)/libexec
 export M2="${M2_HOME}/bin"
 export PYTHONPATH=$(brew --prefix mercurial)/lib/python2.7/site-packages:$(brew --prefix mercurial)/lib/python2.7/site-packages/mercurial:$PYTHONPATH
 
