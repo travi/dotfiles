948a60decdb11edfadfc7fdaa4b135e12b56d6c6
Matt Travi <matt@dwolla.com>
2015-01-27 16:59:22 -0600

    started to extract a meaningful variable from the windows link example for #56

--- windows/link.sh
+++ windows/link.sh
@@ -1,30 +1,31 @@
-# We still need this.
-windows() { [[ -n "$WINDIR" ]]; }
-
 # Cross-platform symlink function. With one parameter, it will check
 # whether the parameter is a symlink. With two parameters, it will create
 # a symlink to a file or directory, with syntax: link $linkname $target
 link() {
-    if [[ -z "$2" ]]; then
+    local link_target=$2
+    local link_name=$1
+
+    # if link_target not provided
+    if [[ -z ${link_target} ]]; then
         # Link-checking mode.
         if windows; then
-            fsutil reparsepoint query "$1" > /dev/null
+            fsutil reparsepoint query ${link_name} > /dev/null
         else
-            [[ -h "$1" ]]
+            [[ -h ${link_name} ]]
         fi
     else
         # Link-creation mode.
         if windows; then
             # Windows needs to be told if it's a directory or not. Infer that.
             # Also: note that we convert `/` to `\`. In this case it's necessary.
-            if [[ -d "$2" ]]; then
+            if [[ -d ${link_target} ]]; then
                 cmd <<< "mklink /D \"$1\" \"${2//\//\\}\"" > /dev/null
             else
                 cmd <<< "mklink \"$1\" \"${2//\//\\}\"" > /dev/null
             fi
         else
             # You know what? I think ln's parameters are backwards.
-            ln -s "$2" "$1"
+            ln -s ${link_target} ${link_name}
         fi
     fi
 }
