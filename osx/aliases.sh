49de4a94a63b3b35f54c1ba511c58489f202c834
Matt Travi <matt@dwolla.com>
2015-02-13 16:09:26 -0600

    changed the rexex operator to a wildcard equivalency check since it is just a begins-with check for #56

--- osx/aliases.sh
+++ osx/aliases.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # OSX-only stuff. Abort if not OSX.
-[[ "$OSTYPE" =~ ^darwin ]] || return 1
+[[ "$OSTYPE" == darwin* ]] || return 1
 
 alias afk="open -a /System/Library/Frameworks/ScreenSaver.framework/Versions/A/Resources/ScreenSaverEngine.app"
 
