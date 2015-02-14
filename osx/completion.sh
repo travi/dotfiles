1ef692052ac44670aeb2c672ad86f387ce0fb3cf
Matt Travi <programmer@travi.org>
2015-02-14 17:13:57 -0600

    fix for shellcheck

--- osx/completion.sh
+++ osx/completion.sh
@@ -3,6 +3,6 @@
 # OSX-only stuff. Abort if not OSX.
 [[ "$OSTYPE" == darwin* ]] || return 1
 
-if [ -f $(brew --prefix)/etc/bash_completion ]; then
-   . $(brew --prefix)/etc/bash_completion
+if [ -f "$(brew --prefix)/etc/bash_completion" ]; then
+   . "$(brew --prefix)/etc/bash_completion"
 fi
