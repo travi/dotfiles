85380bc4d0a325c7e746fd36d7c57711b010f465
Matt Travi <programmer@travi.org>
2015-04-27 21:29:39 -0500

    added mtr for simpler network troubleshooting

--- osx/exports.sh
+++ osx/exports.sh
@@ -3,7 +3,7 @@
 # OSX-only stuff. Abort if not OSX.
 [[ "$OSTYPE" == darwin* ]] || return 1
 
-export PATH=/usr/local/bin:$(brew --prefix ruby)/bin:$PATH
+export PATH=/usr/local/bin:/usr/local/sbin:$(brew --prefix ruby)/bin:$PATH
 export M2_HOME=$(brew --prefix maven)/libexec
 export M2="${M2_HOME}/bin"
 export PYTHONPATH=$(brew --prefix mercurial)/lib/python2.7/site-packages:$(brew --prefix mercurial)/lib/python2.7/site-packages/mercurial:$PYTHONPATH
