bbaf20e2d50621a229bf6a94704eba6ba7ed0f2e
Matt Travi <programmer@travi.org>
2015-06-22 22:15:56 -0500

    wrapped install script in a function

--- test/install.sh
+++ test/install.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-local filename="shellcheck_0.3.7-1_amd64.deb"
-wget "http://ftp.debian.org/debian/pool/main/s/shellcheck/$filename"
-sudo dpkg -i "$filename"
+set -eo pipefail
+
+main() {
+    local filename="shellcheck_0.3.7-1_amd64.deb"
+    wget "http://ftp.debian.org/debian/pool/main/s/shellcheck/$filename"
+    sudo dpkg -i "$filename"
+}
+
+main
