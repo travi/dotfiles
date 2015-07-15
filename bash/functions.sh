262492ce07f679fd083db2aed4d0129c70b0f301
Matt Travi <matt@dwolla.com>
2015-07-15 13:41:10 -0500

    added fallback to netstat for systems where lsof is not available to close #99

--- bash/functions.sh
+++ bash/functions.sh
@@ -2,5 +2,9 @@
 
 # use sudo to check processes not owned by the current user
 port-process() {
-    lsof -i TCP:"$1" | grep LISTEN
+    if [ "$(which lsof)" ]; then
+        lsof -i TCP:"$1" | grep LISTEN
+    else
+        netstat -aon | grep "LISTENING" | grep ":$1"
+    fi
 }
