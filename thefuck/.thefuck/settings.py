fa4978eab216eed4d7e94bc38b631bef0564e0c0
Matt Travi <matt@dwolla.com>
2015-07-17 16:57:49 -0500

    versioned thefuck's settings directory

---
+++ thefuck/.thefuck/settings.py
@@ -0,0 +1,15 @@
+# ~/.thefuck/settings.py: The Fuck settings file
+#
+# The rules are defined as in the example bellow:
+#
+# rules = ['cd_parent', 'git_push', 'python_command', 'sudo']
+#
+# The default values are as follows. Uncomment and change to fit your needs.
+# See https://github.com/nvbn/thefuck#settings for more information.
+#
+
+# rules = []
+# priority = {}
+# require_confirmation = False
+# wait_command = 3
+# no_colors = False
