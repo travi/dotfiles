388dcf89ba9051504888fe3c67462cf87bbbb9b6
Matt Travi <matt@dwolla.com>
2015-07-29 09:34:31 -0500

    added completion for docker

--- bash/completion.sh
+++ bash/completion.sh
@@ -5,4 +5,9 @@ if [[ -e ~/.ssh/known_hosts ]]; then
 	complete -o default -W "$( sed 's/[, ].*//' < ~/.ssh/known_hosts | sort | uniq | grep -v '[0-9]')" ssh scp sftp
 fi
 
+# completion of docker commands
+if [[ "$(which docker)" ]]; then
+    . ~/.dotfiles/docker/bash_completion.sh
+fi
+
 . ~/.dotfiles/osx/completion.sh
