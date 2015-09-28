1a740ef4c17d34de4d0939f3457669fb533caf4b
Matt Travi <programmer@travi.org>
2015-09-27 19:36:34 -0500

    fixed shellcheck errors

--- ruby/rake-completion.sh
+++ ruby/rake-completion.sh
@@ -8,13 +8,13 @@
 _rakecomplete() {
     local cur
     _get_comp_words_by_ref -n : cur
-    rakefile=`find . -maxdepth 1 -iname Rakefile`
+    rakefile=$(find . -maxdepth 1 -iname Rakefile)
     if [ "$rakefile" != "" ]; then
-        recent=`ls -t .rake_tasks~ ${rakefile} **/*.rake 2> /dev/null | head -n 1`
+        recent=$(ls -t .rake_tasks~ "${rakefile}" ./**/*.rake 2> /dev/null | head -n 1)
         if [[ $recent != '.rake_tasks~' ]]; then
             rake --silent --prereqs | grep "rake" | cut -d " " -f 2 > .rake_tasks~
         fi
-        COMPREPLY=($(compgen -W "`cat .rake_tasks~`" -- "$cur"))
+        COMPREPLY=($(compgen -W "$(cat .rake_tasks~)" -- "$cur"))
         # remove colon containing prefix from COMPREPLY items
         __ltrim_colon_completions "$cur"
         return 0
