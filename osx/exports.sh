3881dab98afc09eae6bab2ff12c556157de4ee06
Matt Travi <matt@dwolla.com>
2016-02-29 13:45:46 -0600

    no longer need $SSL_CERT_FILE to be set

--- osx/exports.sh
+++ osx/exports.sh
@@ -7,11 +7,9 @@ export PATH
 export PYTHONPATH
 export M2_HOME
 export M2="${M2_HOME}/bin"
-export SSL_CERT_FILE
 
 PATH=/usr/local/bin:/usr/local/sbin:$(brew --prefix ruby)/bin:~/.local/bin:$PATH
 M2_HOME=$(brew --prefix maven)/libexec
-SSL_CERT_FILE=$(brew --prefix)/etc/openssl/osx_cert.pem
 
 export NVM_DIR=~/.nvm
 
