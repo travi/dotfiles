679014ea597d5868256eb34475351b4cf85fe3b0
Matt Travi <programmer@travi.org>
2016-03-10 22:16:20 -0600

    prefer the rbenv dir under $HOME

--- osx/exports.sh
+++ osx/exports.sh
@@ -7,7 +7,6 @@ export PATH
 export PYTHONPATH
 export M2_HOME
 export M2
-export RBENV_ROOT=/usr/local/var/rbenv
 
 PATH=/usr/local/bin:/usr/local/sbin:$(brew --prefix ruby)/bin:~/.local/bin:$PATH
 M2_HOME=$(brew --prefix maven)/libexec
