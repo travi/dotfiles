03c2032640f1e4ebb045069e8d125468f7e746b5
Matt Travi <matt@dwolla.com>
2016-03-12 00:02:09 -0600

    redirected `which` output to /dev/null for #135

--- setup/software.sh
+++ setup/software.sh
@@ -2,18 +2,18 @@
 
 /bin/bash ../osx/software.sh
 
-if [ "$(which gem)" ]; then
+if which gem > /dev/null; then
     heading 'Ruby Gems'
     gem update
     gem cleanup
 fi
 
-if [ "$(which npm)" ]; then
+if which npm > /dev/null; then
     heading "NPM Update"
     npm update -g
 fi
 
-if [ "$(which vim)" ]; then
+if which vim > /dev/null; then
     heading "Vundle Install"
     vim +PluginInstall! +qall
 fi
