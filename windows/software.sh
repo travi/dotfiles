6b1db15ba77bc64f651b66542d628bb38b7f208c
Matt Travi <matt@dwolla.com>
2016-03-16 09:12:53 -0500

    fixed a few problems with the windows application installation process for #135 and #62

--- windows/software.sh
+++ windows/software.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+. ~/.dotfiles/setup/functions.sh
+
 # Windows-only stuff. Abort if not Windows.
 [[ -n "$WINDIR" ]] || return 1
 
 heading 'Installing/Updating Chocolatey Packages'
-choco installmissing packages.config
-choco update all
+choco install ../windows/packages.config -y
+choco upgrade all -y
