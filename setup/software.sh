75776ada19af739c0fbe3c59766c3c69bb93dae5
Matt Travi <matt@dwolla.com>
2016-03-16 00:09:24 -0500

    redirect sterr output to /dev/null for #135

--- setup/software.sh
+++ setup/software.sh
@@ -3,18 +3,18 @@
 /bin/bash ../osx/software.sh
 /bin/bash ../windows/software.sh
 
-if which gem > /dev/null; then
+if which gem > /dev/null 2>&1; then
     heading 'Ruby Gems'
     gem update
     gem cleanup
 fi
 
-if which npm > /dev/null; then
+if which npm > /dev/null 2>&1; then
     heading "NPM Update"
     npm update -g
 fi
 
-if which vim > /dev/null; then
+if which vim > /dev/null 2>&1; then
     heading "Vundle Install"
     vim +PluginInstall! +qall
 fi
