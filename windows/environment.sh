598cda8a13e507cd1d2f966173bf70a131a9decd
Matt Travi <matt@dwolla.com>
2016-03-19 15:56:25 -0500

    configured the root CA certs for #135

--- windows/environment.sh
+++ windows/environment.sh
@@ -5,3 +5,15 @@
 
 heading 'Configuring ConEmu'
 link_file "$HOME/.dotfiles/windows/ConEmu.xml" "$HOME/AppData/Roaming/ConEmu.xml"
+
+heading 'Configuring Root CA Certificates'
+if [ -e "$HOME/certificates/ca-root/cacert.pem" ]; then
+    info 'already configured'
+else
+    mkdir -p ~/certificates/ca-root
+    if wget --output-document="$HOME/certificates/ca-root/cacert.pem" https://curl.haxx.se/ca/cacert.pem; then
+        success 'successfully configured'
+    else
+        fail 'configuration failed'
+    fi
+fi
--- windows/exports.sh
+++ windows/exports.sh
@@ -7,3 +7,5 @@ export MSYS="winsymlinks:nativestrict"
 
 export PYTHONHOME="/c/tools/python2"
 export PYTHONPATH="/c/tools/python2/Lib"
+
+export SSL_CERT_FILE="~/certificates/ca-root/cacert.pem"
