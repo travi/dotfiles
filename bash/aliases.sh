07978e353c9e76e50d71379e22f97e4073c8801d
Matt Travi <programmer@travi.org>
2016-07-04 23:33:41 -0500

    added command to reload the ~/.inputrc to close #130

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -17,7 +17,7 @@ alias lla="ls -la"
 
 alias hosts='sudo $EDITOR /etc/hosts'
 
-alias reload='. ~/.bash_profile'
+alias reload='. ~/.bash_profile && bind -f ~/.inputrc'
 
 alias ccat="source-highlight --out-format=esc -o STDOUT -i"
 
