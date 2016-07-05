e1f954385098988060b8fc374faa0ee943c4362c
Matt Travi <programmer@travi.org>
2016-07-04 23:11:23 -0500

    added a bash function to look up http status codes

--- bash/functions.sh
+++ bash/functions.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
 # use sudo to check processes not owned by the current user
-port-process() {
+port_process() {
     if [ "$(which lsof)" ]; then
         lsof -i TCP:"$1" | grep LISTEN
     else
         netstat -aon | grep "LISTENING" | grep ":$1"
     fi
+}
+
+http(){
+    curl http://httpcode.info/$1;
 }
