14b5a7906e791314b977db8c4a3647be78b47543
Matt Travi <matt@gaincompliance.com>
2016-11-29 23:30:56 -0600

    fixed shellcheck error

--- bash/colors.sh
+++ bash/colors.sh
@@ -77,6 +77,6 @@ export LS_COLORS='no=00:fi=00:di=01;34:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40
 
 # aliases for GRC from homebrew
 if grc &>/dev/null && brew --version &>/dev/null; then
-    #shellcheck disable=SC1090
+    #shellcheck disable=SC1091
     source "/usr/local/etc/grc.bashrc"
 fi
