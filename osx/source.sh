9fac159b3079b91bea33c18f6b0169f80af3542f
Matt Travi <matt@gaincompliance.com>
2018-01-30 14:26:09 -0600

    explicitly referenced the nvm path
    based on the workaround mentioned here: https://github.com/creationix/nvm/issues/617#issuecomment-358672415

--- osx/source.sh
+++ osx/source.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+#shellcheck disable=SC1090
 
 # OSX-only stuff. Abort if not OSX.
 [[ "$OSTYPE" == darwin* ]] || return 1
 
-#shellcheck disable=SC1090
-source "$(brew --prefix nvm)/nvm.sh"
-#shellcheck disable=SC1090
 source ~/.files/gpg/init.sh
-#shellcheck disable=SC1090
+
+# source "$(brew --prefix nvm)/nvm.sh"
+export NVM_DIR="$(realpath $HOME/.nvm)"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+
 [[ -s ~/.avn/bin/avn.sh ]] && source ~/.avn/bin/avn.sh
