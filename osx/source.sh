88f864678262650ea6a4a9b25334614d99ee13f5
Matt Travi <matt@gaincompliance.com>
2018-04-06 11:56:43 -0500

    sourced completion provided from brew

--- osx/source.sh
+++ osx/source.sh
@@ -11,3 +11,5 @@ export NVM_DIR="$(realpath $HOME/.nvm)"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 
 [[ -s ~/.avn/bin/avn.sh ]] && source ~/.avn/bin/avn.sh
+
+source "$(brew --prefix)/etc/bash_completion"
