c6a82d346757c0d2f024d877a74701d23735fb87
Matt Travi <programmer@travi.org>
2018-11-13 00:39:31 -0600

    updated the directory added to the PATH for gpg (agent)

--- osx/exports.sh
+++ osx/exports.sh
@@ -7,7 +7,7 @@ export PATH
 export M2_HOME
 export M2
 
-PATH=/usr/local/bin:/usr/local/sbin:$(brew --prefix gpg-agent)/bin:$(brew --prefix ruby)/bin:$(brew --prefix python)/libexec/bin:~/.local/bin:$PATH
+PATH=/usr/local/bin:/usr/local/sbin:$(brew --prefix gnupg)/bin:$(brew --prefix ruby)/bin:$(brew --prefix python)/libexec/bin:~/.local/bin:$PATH
 M2_HOME=$(brew --prefix maven)/libexec
 M2="${M2_HOME}/bin"
 
