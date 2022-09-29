b82c32411ddab91cc96ff6d14a691546c17f6fb1
Matt Travi <programmer@travi.org>
2022-09-29 11:45:18 -0500

    fix(brew): updated the path to work with modern homebrew

--- osx/exports.sh
+++ osx/exports.sh
@@ -7,7 +7,8 @@ export PATH
 export M2_HOME
 export M2
 
-PATH=/usr/local/bin:/usr/local/sbin:$(brew --prefix gnupg)/bin:$(brew --prefix ruby)/bin:$(brew --prefix python)/libexec/bin:~/.local/bin:$PATH
+PATH=/usr/local/bin:/usr/local/sbin:/opt/homebrew/bin:/opt/homebrew/sbin:$PATH
+PATH=$(brew --prefix gnupg)/bin:$(brew --prefix ruby)/bin:$(brew --prefix python)/libexec/bin:~/.local/bin:$PATH
 M2_HOME=$(brew --prefix maven)/libexec
 M2="${M2_HOME}/bin"
 
